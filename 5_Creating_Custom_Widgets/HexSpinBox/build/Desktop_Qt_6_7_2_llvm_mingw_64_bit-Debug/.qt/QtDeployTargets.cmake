5c6c2893558160f0af2ee9eba7578ecc38bc8688
Jiuso <jesusgg200x@gmail.com>
2024-11-23 01:46:56 +0100

    IconEditor started.

---
+++ 5_Creating_Custom_Widgets/HexSpinBox/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_HexSpinBox_FILE C:/Users/jesus/Documents/c++/proyectosQt/C++_GUI_Programming_with_Qt_4/5_Creating_Custom_Widgets/HexSpinBox/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/HexSpinBox.exe)
+set(__QT_DEPLOY_TARGET_HexSpinBox_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_HexSpinBox_RUNTIME_DLLS C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
---
+++ 5_Creating_Custom_Widgets/IconEditor/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_IconEditor_FILE C:/Users/jesus/Documents/c++/proyectosQt/C++_GUI_Programming_with_Qt_4/5_Creating_Custom_Widgets/IconEditor/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/IconEditor.exe)
+set(__QT_DEPLOY_TARGET_IconEditor_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_IconEditor_RUNTIME_DLLS C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
