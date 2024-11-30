47a36f75fa10c88458fbe377e7d167deb2d17eed
Jiuso <jesusgg200x@gmail.com>
2024-11-30 13:57:24 +0100

    IconEditor finished.

--- 5_Creating_Custom_Widgets/IconEditor/iconeditor.cpp
+++ 5_Creating_Custom_Widgets/IconEditor/iconeditor.cpp
@@ -32,3 +32,83 @@ void IconEditor::setIconImage(const QImage &newImage){
         updateGeometry();
     }
 }
+
+void IconEditor::setZoomFactor(int newZoom){
+    if(newZoom < 1){
+        newZoom = 1;
+    }
+
+    if(newZoom != zoom){
+        zoom = newZoom;
+        update();
+        updateGeometry();
+    }
+}
+
+void IconEditor::paintEvent(QPaintEvent *event){
+    QPainter painter(this);
+
+    if(zoom >= 3){
+        painter.setPen(Qt::black);//The book uses palette().foreground().color(), but foreground() method doesn't exist in Qt 6.7.2. Therefore, we've used Qt::black.
+        for(int i = 0;i <= image.width();i++){
+            painter.drawLine(zoom * i, 0,
+                             zoom * i, zoom * image.height());
+        }
+
+        for(int j = 0;j <= image.height();j++){
+            painter.drawLine(0, zoom * j,
+                             zoom * image.width(), zoom * j);
+        }
+    }
+
+    for(int i = 0;i < image.width();i++){
+        for(int j = 0;j < image.height();j++){
+            QRect rect = pixelRect(i, j);
+            if(!event->region().intersected(rect).isEmpty()){
+                QColor color = QColor::fromRgba(image.pixel(i, j));
+                if(color.alpha() < 255){
+                    painter.fillRect(rect, Qt::white);
+                }
+                painter.fillRect(rect, color);
+            }
+        }
+    }
+}
+
+QRect IconEditor::pixelRect(int i, int j) const{
+    if(zoom >= 3){
+        return QRect(zoom * i + 1, zoom * j + 1, zoom - 1, zoom - 1);
+    }else{
+        return QRect(zoom * i, zoom * j, zoom, zoom);
+    }
+}
+
+void IconEditor::mousePressEvent(QMouseEvent *event){
+    if(event->button() == Qt::LeftButton){
+        setImagePixel(event->pos(), true);
+    }else if(event->button() == Qt::RightButton){
+        setImagePixel(event->pos(), false);
+    }
+}
+
+void IconEditor::mouseMoveEvent(QMouseEvent *event){
+    if(event->buttons() & Qt::LeftButton){//Bitwise OR.
+        setImagePixel(event->pos(), true);
+    }else if(event->buttons() & Qt::RightButton){//Bitwise OR.
+        setImagePixel(event->pos(), false);
+    }
+}
+
+void IconEditor::setImagePixel(const QPoint &pos, bool opaque){
+    int i = pos.x() / zoom;
+    int j = pos.y() / zoom;
+
+    if(image.rect().contains(i, j)){
+        if(opaque){
+            image.setPixel(i, j, penColor().rgba());
+        }else{
+            image.setPixel(i, j, qRgba(0, 0, 0, 0));
+        }
+        update(pixelRect(i, j));
+    }
+}
--- 5_Creating_Custom_Widgets/IconEditor/main.cpp
+++ 5_Creating_Custom_Widgets/IconEditor/main.cpp
@@ -1,10 +1,11 @@
-//#include "mainwindow.h"
+#include "iconeditor.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+    IconEditor *iconEditor = new IconEditor;
+    iconEditor->show();
     return a.exec();
 }
